ed53b4ad4113e406dd15162f33197f44759e37a3
Eddie Beller <>
2022-01-26 15:36:13 +0300

    :update: bugfix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -81,18 +81,74 @@ document.querySelector("#nav-icon").addEventListener("click", () => {
 
 // Функция ymaps.ready() будет вызвана, когда
 // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+// ymaps.ready(init);
+// function init() {
+//   // Создание карты.
+//   var myMap = new ymaps.Map("map", {
+//     // Координаты центра карты.
+//     // Порядок по умолчанию: «широта, долгота».
+//     // Чтобы не определять координаты центра карты вручную,
+//     // воспользуйтесь инструментом Определение координат.
+//     center: [45.03547, 38.975313],
+//     // Уровень масштабирования. Допустимые значения:
+//     // от 0 (весь мир) до 19.
+//     zoom: 12,
+//     controls: ["zoomControl"],
+//   });
+//   // Создаем геообъект с типом геометрии "Точка".
+//   (myGeoObject = new ymaps.GeoObject({
+//     // Описание геометрии.
+//     geometry: {
+//       type: "Point",
+//       coordinates: [45.03547, 38.975313],
+//     },
+//     // Свойства.
+//     properties: {
+//       // Контент метки.
+//       // iconContent: "Я тащусь",
+//       // hintContent: "Ну давай уже тащи",
+//     },
+//   })),
+//     {
+//       // Опции.
+//       // Иконка метки будет растягиваться под размер ее содержимого.
+//       preset: "islands#blackStretchyIcon",
+//       // Метку можно перемещать.
+//       draggable: true,
+//     };
+// }
+
 ymaps.ready(init);
+
 function init() {
-  // Создание карты.
-  var myMap = new ymaps.Map("map", {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
-    center: [45.03547, 38.975313],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
-    zoom: 12,
-    controls: ["zoomControl"],
-  });
+  var myMap = new ymaps.Map(
+      "map",
+      {
+        center: [45.063432, 38.995714],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 11,
+        controls: ["zoomControl"],
+      },
+      {
+        searchControlProvider: "yandex#search",
+      }
+    ),
+    myGeoObject = new ymaps.GeoObject({
+      geometry: {
+        type: "Point",
+      },
+    });
+  myMap.geoObjects.add(myGeoObject).add(
+    new ymaps.Placemark(
+      [45.063432, 38.995714],
+      {
+        balloonContent:
+          "г. Краснодар, ул. Зиповская 5. <br> V&I Models Agency",
+      },
+      {
+        preset: "islands#blackStretchyIcon",
+      }
+    )
+  );
 }
